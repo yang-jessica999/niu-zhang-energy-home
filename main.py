84676d28343dccf87c47abe58f2b6dd83d8b2750
yang-jessica999 <ab680814@gmail.com>
2025-04-13 22:36:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, abort
-import openai
 import os
+import openai
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
@@ -11,17 +11,24 @@ line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@app.route("/callback", methods=["POST"])
+@app.route("/", methods=["GET"])
+def index():
+    return "LINE Bot is running!"
+
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers.get('X-Line-Signature')
     body = request.get_data(as_text=True)
+
+    if signature is None:
+        abort(400)
 
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
-    return "OK"
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -30,7 +37,7 @@ def handle_message(event):
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": user_msg}]
     )
-    reply = response["choices"][0]["message"]["content"].strip()
+    reply = response['choices'][0]['message']['content'].strip()
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply)
